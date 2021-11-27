0d7691f9d2a578c310e2e6299a86fd01eda4545c
Aleks <aleksskela591@gmail.com>
2021-11-27 15:19:22 +0100

    square (n) sum

---
+++ src/main/java/com/codewars/eightkyu/SquareNSum.java
@@ -0,0 +1,13 @@
+package com.codewars.eightkyu;
+
+/*
+Complete the square sum function so that it squares each number passed into it and then sums the results together.
+For example, for [1, 2, 2] it should return 9 because 1^2 + 2^2 + 2^2 = 9.
+ */
+
+public class SquareNSum {
+    public static int squareSum(int[] n) {
+
+        return 0;
+    }
+}
