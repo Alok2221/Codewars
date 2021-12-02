8020207a65fb46f8fe6d450e205a5270fedde210
Aleks <aleksskela591@gmail.com>
2021-12-02 22:23:40 +0100

    convert a boolean to a String

---
+++ src/main/java/com/codewars/eightkyu/ConvertABooleanToAString.java
@@ -0,0 +1,15 @@
+package com.codewars.eightkyu;
+
+//Implement a function which convert the given boolean value into its string representation.
+//        Note: Only valid inputs will be given.
+
+public class ConvertABooleanToAString {
+
+    public static String convert(boolean b){
+        if(true) {
+            return "true";
+        } else {
+            return "false";
+        }
+    }
+}
