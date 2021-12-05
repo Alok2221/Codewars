ffa4dc4079635eef606c594b97db58a1c6110eac
Aleks <aleksskela591@gmail.com>
2021-12-05 15:15:54 +0100

    convert a number to a String

---
+++ src/main/java/com/codewars/eightkyu/ConvertANumberToAString.java
@@ -0,0 +1,13 @@
+package com.codewars.eightkyu;
+
+//We need a function that can transform a number into a string.
+//        What ways of achieving this do you know?
+//        Examples:
+//        123 --> "123"
+//        999 --> "999"
+
+public class ConvertANumberToAString {
+    public static String numberToString(int num) {
+        return null;
+    }
+}
