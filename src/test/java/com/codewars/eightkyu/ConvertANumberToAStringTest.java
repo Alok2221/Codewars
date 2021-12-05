900d0e03c35828d291bb34594fa018ed6393e3b5
Aleks <aleksskela591@gmail.com>
2021-12-05 15:18:49 +0100

    add test

---
+++ src/test/java/com/codewars/eightkyu/ConvertANumberToAStringTest.java
@@ -0,0 +1,15 @@
+package com.codewars.eightkyu;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class ConvertANumberToAStringTest {
+    @Test
+    public void tests() {
+        assertEquals("67", ConvertANumberToAString.numberToString(67));
+        assertEquals("123", ConvertANumberToAString.numberToString(123));
+        assertEquals("999", ConvertANumberToAString.numberToString(999));
+    }
+
+}
