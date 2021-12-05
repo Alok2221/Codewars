7a3dea9a061132f4d6f3d57dfd4ed51ac9fa24de
Aleks <aleksskela591@gmail.com>
2021-12-05 20:00:10 +0100

    convert a string to an array

---
+++ src/main/java/com/codewars/eightkyu/ConvertAStringToAnArray.java
@@ -0,0 +1,12 @@
+package com.codewars.eightkyu;
+
+//Write a function to split a string and convert it into an array of words. For example:
+//        "Robin Singh" ==> ["Robin", "Singh"]
+//        "I love arrays they are my favorite" ==> ["I", "love", "arrays", "they", "are", "my", "favorite"]
+
+public class ConvertAStringToAnArray {
+
+    public static String[] stringToArray(String s) {
+        return null;
+    }
+}
