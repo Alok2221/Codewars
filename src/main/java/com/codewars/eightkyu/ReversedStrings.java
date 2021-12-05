30523cebb1b20093c0d2ac60ff59620f6cf0ce45
Aleks <aleksskela591@gmail.com>
2021-12-05 15:04:39 +0100

    reversed Strings

---
+++ src/main/java/com/codewars/eightkyu/ReversedStrings.java
@@ -0,0 +1,12 @@
+package com.codewars.eightkyu;
+
+//Complete the solution so that it reverses the string passed into it.
+//        'world'  =>  'dlrow'
+//        'word'   =>  'drow'
+
+public class ReversedStrings {
+    public static String solution(String str) {
+        // Your code here...
+        return "";
+    }
+}
