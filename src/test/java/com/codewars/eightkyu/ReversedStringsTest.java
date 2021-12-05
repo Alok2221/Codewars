997ac42d19588df3b9000e3c33587de9a948e944
Aleks <aleksskela591@gmail.com>
2021-12-05 15:04:50 +0100

    add test

---
+++ src/test/java/com/codewars/eightkyu/ReversedStringsTest.java
@@ -0,0 +1,12 @@
+package com.codewars.eightkyu;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class ReversedStringsTest {
+    @Test
+    public void sampleTests() {
+        assertEquals("dlrow", ReversedStrings.solution("world"));
+    }
+}
