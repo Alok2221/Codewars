78b5aa3a2effd5628a68237504c96f073e223a16
Aleks <aleksskela591@gmail.com>
2021-12-05 15:54:22 +0100

    add test

---
+++ src/test/java/com/codewars/eightkyu/RemoveFirstAndLastCharacterTest.java
@@ -0,0 +1,17 @@
+package com.codewars.eightkyu;
+
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class RemoveFirstAndLastCharacterTest {
+
+    @Test
+    public void testRemoval() {
+        assertEquals("loquen", RemoveFirstAndLastCharacter.remove("eloquent"));
+        assertEquals("ountr", RemoveFirstAndLastCharacter.remove("country"));
+        assertEquals("erso", RemoveFirstAndLastCharacter.remove("person"));
+        assertEquals("lac", RemoveFirstAndLastCharacter.remove("place"));
+    }
+}
