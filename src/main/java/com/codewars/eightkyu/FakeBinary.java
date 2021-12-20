df50c85d2fa4e01777c39a753f3477602dc4cf2c
Aleks <aleksskela591@gmail.com>
2021-12-20 23:43:34 +0100

    fake binary

---
+++ src/main/java/com/codewars/eightkyu/FakeBinary.java
@@ -0,0 +1,11 @@
+package com.codewars.eightkyu;
+
+//Given a string of digits, you should replace any digit below 5 with '0' and any digit 5 and above with '1'.
+// Return the resulting string.
+//        Note: input will never be an empty string
+
+public class FakeBinary {
+    public static String fakeBin(String numberString) {
+        return "";
+    }
+}
