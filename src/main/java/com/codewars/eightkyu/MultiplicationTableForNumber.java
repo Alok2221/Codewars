261b0906e8187db077aac8de6d761418393e4b8b
Aleks <aleksskela591@gmail.com>
2021-12-20 22:45:17 +0100

    multiplication table for number solution

--- src/main/java/com/codewars/eightkyu/MultiplicationTableForNumber.java
+++ src/main/java/com/codewars/eightkyu/MultiplicationTableForNumber.java
@@ -19,6 +19,11 @@ package com.codewars.eightkyu;
 
 public class MultiplicationTableForNumber {
     public static String multiTable(int num) {
-        return "";
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i <= 10; i++) {
+            int result = i * num;
+            sb.append(i).append(" * ").append(num).append(" = ").append(result).append("\n");
+        }
+        return sb.toString().trim();
     }
 }
