729f2b2a68e6c73fc9fa4434991a23a68d33b078
Aleks <aleksskela591@gmail.com>
2022-01-07 13:30:44 +0100

    solution

--- src/main/java/com/codewars/eightkyu/RockPaperScissors.java
+++ src/main/java/com/codewars/eightkyu/RockPaperScissors.java
@@ -9,6 +9,20 @@ package com.codewars.eightkyu;
 
 public class RockPaperScissors {
     public static String rps(String p1, String p2) {
-        return null;
+        if (p1.equals("rock") && p2.equals("scissors")) {
+            return "Player 1 won!";
+        } else if (p1.equals("rock") && p2.equals("paper")) {
+            return "Player 2 won!";
+        } else if (p1.equals("scissors") && p2.equals("paper")) {
+            return "Player 1 won!";
+        } else if (p1.equals("scissors") && p2.equals("rock")) {
+            return "Player 2 won!";
+        } else if (p1.equals("paper") && p2.equals("rock")) {
+            return "Player 1 won!";
+        } else if (p1.equals("paper") && p2.equals("scissors")) {
+            return "Player 2 won!";
+        } else {
+            return "Draw!";
+        }
     }
 }
