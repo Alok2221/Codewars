07d8ebc9f845dd7b22b6fdf0099d8bf9fe100860
Aleks <aleksskela591@gmail.com>
2022-01-07 13:37:05 +0100

    make upper case

---
+++ src/main/java/com/codewars/eightkyu/MakeUpperCase.java
@@ -0,0 +1,11 @@
+package com.codewars.eightkyu;
+
+//Write a function which converts the input string to uppercase.
+
+import java.util.Locale;
+
+public class MakeUpperCase {
+    public static String MakeUpperCase(String str){
+    return str;
+    }
+}
