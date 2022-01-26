8ae5f242b4198c10533402a2cc7977ee5b04f722
Aleks <aleksskela591@gmail.com>
2022-01-26 12:27:26 +0100

    remove consecutive duplicate words

---
+++ src/main/java/com/codewars/sevenkyu/RemoveConsecutiveDuplicateWords.java
@@ -0,0 +1,11 @@
+package com.codewars.sevenkyu;
+
+//Your task is to remove all consecutive duplicate words from a string, leaving only first words entries. For example:
+//        "alpha beta beta gamma gamma gamma delta alpha beta beta gamma gamma gamma delta"
+//        --> "alpha beta gamma delta alpha beta gamma delta"
+
+public class RemoveConsecutiveDuplicateWords {
+    public static String removeConsecutiveDuplicates(String s){
+        return s;
+    }
+}
