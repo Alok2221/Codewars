78b40c54243a789cadaf65df7e91e0730453432c
Aleks <aleksskela591@gmail.com>
2022-01-26 12:59:21 +0100

    double every other

---
+++ src/main/java/com/codewars/sevenkyu/DoubleEveryOther.java
@@ -0,0 +1,14 @@
+package com.codewars.sevenkyu;
+
+//      Write a function that doubles every second integer in a list, starting from the left.
+//      Example:
+//      For input array/list :
+//      [1,2,3,4]
+//      the function should return :
+//      [1,4,3,8]
+
+public class DoubleEveryOther {
+    public static int[] doubleEveryOther(int[] a){
+        return a;
+    }
+}
