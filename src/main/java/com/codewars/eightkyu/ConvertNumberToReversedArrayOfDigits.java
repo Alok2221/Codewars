62c73d8368d9a1bc6115d6714367986af0bc4dd8
Aleks <aleksskela591@gmail.com>
2022-01-28 12:40:23 +0100

    convert number to reversed array of digits

---
+++ src/main/java/com/codewars/eightkyu/ConvertNumberToReversedArrayOfDigits.java
@@ -0,0 +1,14 @@
+package com.codewars.eightkyu;
+
+//      Convert number to reversed array of digits
+//      Given a random non-negative number, you have to return the digits of this number within an array in reverse order.
+//
+//      Example:
+//      348597 => [7,9,5,8,4,3]
+//      0 => [0]
+
+public class ConvertNumberToReversedArrayOfDigits {
+    public static int[] digitize(long n) {
+        return new int[0];
+    }
+}
