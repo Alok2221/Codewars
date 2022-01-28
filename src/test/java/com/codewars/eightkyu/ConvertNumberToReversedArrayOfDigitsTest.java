387f941458bc883203cd14d176b1f2f8b693f40b
Aleks <aleksskela591@gmail.com>
2022-01-28 12:40:30 +0100

    add test

---
+++ src/test/java/com/codewars/eightkyu/ConvertNumberToReversedArrayOfDigitsTest.java
@@ -0,0 +1,13 @@
+package com.codewars.eightkyu;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertArrayEquals;
+
+public class ConvertNumberToReversedArrayOfDigitsTest {
+    @Test
+    public void tests() {
+        assertArrayEquals(new int[]{1, 3, 2, 5, 3}, ConvertNumberToReversedArrayOfDigits.digitize(35231));
+        assertArrayEquals(new int[]{0}, ConvertNumberToReversedArrayOfDigits.digitize(0));
+    }
+}
