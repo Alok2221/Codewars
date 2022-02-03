5445b2e6d8d1e4c53f3613bc54b1cef0a323ad0a
Aleks <aleksskela591@gmail.com>
2022-02-03 15:59:47 +0100

    solution

--- src/main/java/com/codewars/eightkyu/ANeedleInTheHaystack.java
+++ src/main/java/com/codewars/eightkyu/ANeedleInTheHaystack.java
@@ -11,6 +11,11 @@ import java.util.Arrays;
 public class ANeedleInTheHaystack {
 
     public static String findNeedle(Object[] haystack) {
+        for (int i = 0; i < haystack.length; i++) {
+            if (haystack[i]=="needle") {
+                return "found the needle at position " + i;
+            }
+        }
         return null;
     }
 }
