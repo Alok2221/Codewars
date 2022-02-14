e9d1fbf477689f5bce3096bfee2cbced6bd485b9
Aleks <aleksskela591@gmail.com>
2022-02-14 15:17:10 +0100

    get character from ascii value

---
+++ src/main/java/com/codewars/eightkyu/GetCharacterFromAsciiValue.java
@@ -0,0 +1,13 @@
+package com.codewars.eightkyu;
+
+//      Write a function get_char() / getChar() which takes a number and returns the corresponding ASCII char for that value.
+//      Example:
+//      get_char(65)
+//      should return:
+//      'A'
+
+public class GetCharacterFromAsciiValue {
+    public static char getChar(int c) {
+        return 0;
+    }
+}
