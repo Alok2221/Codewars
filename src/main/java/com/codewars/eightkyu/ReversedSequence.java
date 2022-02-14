7965dcb5b8f5615fdedf319e06f66b1bbd6f1b22
Aleks <aleksskela591@gmail.com>
2022-02-14 11:23:00 +0100

    reversed sequence

---
+++ src/main/java/com/codewars/eightkyu/ReversedSequence.java
@@ -0,0 +1,11 @@
+package com.codewars.eightkyu;
+
+//Build a function that returns an array of integers from n to 1 where n>0.
+//        Example : n=5 --> [5,4,3,2,1]
+
+public class ReversedSequence {
+    public static int[] reverse(int n){
+        //your code
+        return new int[]{};
+    }
+}
