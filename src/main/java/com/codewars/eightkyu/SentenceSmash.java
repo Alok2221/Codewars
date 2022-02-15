8d22ab5fa43e5421b504a7ba79be571f3c4be99b
Aleks <aleksskela591@gmail.com>
2022-02-15 15:06:05 +0100

    solution

--- src/main/java/com/codewars/eightkyu/SentenceSmash.java
+++ src/main/java/com/codewars/eightkyu/SentenceSmash.java
@@ -8,7 +8,6 @@ package com.codewars.eightkyu;
 
 public class SentenceSmash {
     public static String smash(String... words) {
-        // TODO Write your code below this comment please
-        return "";
+        return String.join(" ", words);
     }
 }
