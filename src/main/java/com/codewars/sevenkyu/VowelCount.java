48ace93a9d5abdba89b6282deb18ba822fa909f5
Aleks <aleksskela591@gmail.com>
2022-02-16 20:20:03 +0100

    solution

--- src/main/java/com/codewars/sevenkyu/VowelCount.java
+++ src/main/java/com/codewars/sevenkyu/VowelCount.java
@@ -6,6 +6,12 @@ package com.codewars.sevenkyu;
 
 public class VowelCount {
     public static int getCount(String str) {
-        return 0;
+        int count = 0;
+        for (int i = 0; i < str.length(); i++) {
+            if ((str.charAt(i) == 'a') || (str.charAt(i) == 'e') || (str.charAt(i) == 'i') || (str.charAt(i) == 'o') || (str.charAt(i) == 'u')) {
+                count++;
+            }
+        }
+        return count;
     }
 }
