89edc0dd0c15c081c10ba1cfec8332c36e992e98
Aleks <aleksskela591@gmail.com>
2022-02-22 11:42:43 +0100

    solution

--- src/main/java/com/codewars/eightkyu/QuarterOfTheYear.java
+++ src/main/java/com/codewars/eightkyu/QuarterOfTheYear.java
@@ -6,6 +6,17 @@ package com.codewars.eightkyu;
 
 public class QuarterOfTheYear {
     public static int quarterOf(int month) {
-        return 0;
+        int quarter = 0;
+
+        if (month <= 3) {
+            quarter = 1;
+        } else if (month <= 6) {
+            quarter = 2;
+        } else if (month <= 9) {
+            quarter = 3;
+        } else if (month <= 12) {
+            quarter = 4;
+        }
+        return quarter;
     }
 }
