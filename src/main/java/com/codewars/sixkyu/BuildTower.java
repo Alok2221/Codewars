a8ef20ec710c67fef6301ae3ad976dea83fb54a4
Aleks <aleksskela591@gmail.com>
2022-02-23 23:40:31 +0100

    build bower

---
+++ src/main/java/com/codewars/sixkyu/BuildTower.java
@@ -0,0 +1,31 @@
+package com.codewars.sixkyu;
+
+//      Build Tower
+//      Build Tower by the following given argument:
+//      number of floors (integer and always greater than 0).
+//      Tower block is represented as *
+
+//      for example, a tower of 3 floors looks like below
+//
+//        [
+//        "  *  ",
+//        " *** ",
+//        "*****"
+//        ]
+//        and a tower of 6 floors looks like below
+//
+//        [
+//        "     *     ",
+//        "    ***    ",
+//        "   *****   ",
+//        "  *******  ",
+//        " ********* ",
+//        "***********"
+//        ]
+
+public class BuildTower {
+    public static String[] TowerBuilder(int nFloors) {
+        String[] tower = new String[nFloors];
+        return new String[0];
+    }
+}
