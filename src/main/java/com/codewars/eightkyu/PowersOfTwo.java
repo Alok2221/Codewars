e6b1df601e7886f928b26ee9e1ecbb3790e51aad
Aleks <aleksskela591@gmail.com>
2022-02-26 22:51:34 +0100

    powers of two

---
+++ src/main/java/com/codewars/eightkyu/PowersOfTwo.java
@@ -0,0 +1,14 @@
+package com.codewars.eightkyu;
+
+//      Complete the function that takes a non-negative integer n as input, and returns a list of all the powers of 2 with the exponent ranging from 0 to n (inclusive).
+//
+//      Examples
+//      n = 0  ==> [1]        # [2^0]
+//      n = 1  ==> [1, 2]     # [2^0, 2^1]
+//      n = 2  ==> [1, 2, 4]  # [2^0, 2^1, 2^2]
+
+public class PowersOfTwo {
+    public static long[] powersOfTwo(int n){
+        return new long[0];
+    }
+}
