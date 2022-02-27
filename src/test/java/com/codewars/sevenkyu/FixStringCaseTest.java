b13675e203caaf44b474616e00f88bd7239d8c6c
Aleks <aleksskela591@gmail.com>
2022-02-27 22:01:50 +0100

    add test

---
+++ src/test/java/com/codewars/sevenkyu/FixStringCaseTest.java
@@ -0,0 +1,16 @@
+package com.codewars.sevenkyu;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class FixStringCaseTest {
+    @Test
+    public void BasicTests() {
+        assertEquals("code", FixStringCase.solve("code"));
+        assertEquals("CODE", FixStringCase.solve("CODe"));
+        assertEquals("code", FixStringCase.solve("COde"));
+        assertEquals("code", FixStringCase.solve("Code"));
+        assertEquals("", FixStringCase.solve(""));
+    }
+}
