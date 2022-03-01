1dfe4423ad0238c985f8582525c2100397e3e4fe
Aleks <aleksskela591@gmail.com>
2022-03-01 22:19:52 +0100

    solution

--- src/main/java/com/codewars/sixkyu/BouncingBalls.java
+++ src/main/java/com/codewars/sixkyu/BouncingBalls.java
@@ -19,6 +19,13 @@ package com.codewars.sixkyu;
 
 public class BouncingBalls {
     public static int bouncingBall(double h, double bounce, double window) {
-        return 0;
+        if (h <= 0 || bounce <= 0 || bounce >= 1 || window >= h) return -1;
+        double bounceHeight = h * bounce;
+        int count = 1;
+        while (bounceHeight > window) {
+            count += 2;
+            bounceHeight = bounceHeight * bounce;
+        }
+        return count;
     }
 }
