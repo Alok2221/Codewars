c44c28b241d207a673182661dfc1e331f878f644
Aleks <aleksskela591@gmail.com>
2022-03-04 14:57:03 +0100

    solution

--- src/main/java/com/codewars/sevenkyu/LargestFiveDigitNumberInASeries.java
+++ src/main/java/com/codewars/sevenkyu/LargestFiveDigitNumberInASeries.java
@@ -14,6 +14,11 @@ package com.codewars.sevenkyu;
 
 public class LargestFiveDigitNumberInASeries {
     public static int solve(final String digits) {
-        return 0;
+        int largest = Integer.MIN_VALUE;
+        for (int i = 0; i <= digits.length() - 5; i++) {
+            int number = Integer.parseInt(digits.substring(i, i + 5));
+            largest = Math.max(number, largest);
+        }
+        return largest;
     }
 }
