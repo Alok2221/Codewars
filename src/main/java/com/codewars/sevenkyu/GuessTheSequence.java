3c688b37279b04a2b91d1505716b3019cdc3b0af
Aleks <aleksskela591@gmail.com>
2022-03-07 22:35:41 +0100

    solution

--- src/main/java/com/codewars/sevenkyu/GuessTheSequence.java
+++ src/main/java/com/codewars/sevenkyu/GuessTheSequence.java
@@ -5,8 +5,10 @@ package com.codewars.sevenkyu;
 //      x = 16
 //      result = [1, 10, 11, 12, 13, 14, 15, 16, 2, 3, 4, 5, 6, 7, 8, 9]
 
+import java.util.stream.IntStream;
+
 public class GuessTheSequence {
     static int[] sequence(int x) {
-        return new int[0];
+        return IntStream.rangeClosed(1, x).mapToObj(Integer::toString).sorted().mapToInt(Integer::parseInt).toArray();
     }
 }
