2ad07c6a2c2844106342be6e39a2d2af19175795
Aleks <aleksskela591@gmail.com>
2022-03-11 17:14:50 +0100

    easy line

---
+++ src/main/java/com/codewars/sevenkyu/EasyLine.java
@@ -0,0 +1,22 @@
+package com.codewars.sevenkyu;
+
+//      In the drawing below we have a part of the Pascal's triangle, horizontal lines are numbered from zero (top).
+//      We want to calculate the sum of the squares of the binomial coefficients on a given horizontal line
+//      with a function called easyline (or easyLine or easy-line).
+//      Can you write a program which calculate easyline(n) where n is the horizontal line number?
+//      The function will take n (with: n>= 0) as parameter and will
+//      return the sum of the squares of the binomial coefficients on line n.
+//
+//      Examples:
+//      easyline(0) => 1
+//      easyline(1) => 2
+//      easyline(4) => 70
+//      easyline(50) => 100891344545564193334812497256
+
+import java.math.BigInteger;
+
+public class EasyLine {
+    public static BigInteger easyLine(int n) {
+        return new BigInteger("000");
+    }
+}
