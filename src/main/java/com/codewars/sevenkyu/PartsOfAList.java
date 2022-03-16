32cc2370f8ff104c92f9dab78ead64671ceb790b
Aleks <aleksskela591@gmail.com>
2022-03-16 11:14:11 +0100

    solution

--- src/main/java/com/codewars/sevenkyu/PartsOfAList.java
+++ src/main/java/com/codewars/sevenkyu/PartsOfAList.java
@@ -20,8 +20,15 @@ package com.codewars.sevenkyu;
 //      a = ["az", "toto", "picaro", "zone", "kiwi"] -->
 //      "(az, toto picaro zone kiwi)(az toto, picaro zone kiwi)(az toto picaro, zone kiwi)(az toto picaro zone, kiwi)"
 
+import java.util.Arrays;
+
 public class PartsOfAList {
     public static String[][] partlist(String[] arr) {
-        return new String[][]{};
+        String[][] returnArray = new String[arr.length - 1][2];
+        for (int i = 0; i < arr.length - 1; ++i) {
+            returnArray[i][0] = String.join(" ", Arrays.copyOfRange(arr, 0, i + 1));
+            returnArray[i][1] = String.join(" ", Arrays.copyOfRange(arr, i + 1, arr.length));
+        }
+        return returnArray;
     }
 }
