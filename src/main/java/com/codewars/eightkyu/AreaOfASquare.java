e381fba7a1778cd4f1a23dc120c08a9cdb39e3e3
Aleks <aleksskela591@gmail.com>
2022-03-18 09:12:37 +0100

    area of a square

---
+++ src/main/java/com/codewars/eightkyu/AreaOfASquare.java
@@ -0,0 +1,11 @@
+package com.codewars.eightkyu;
+
+//      Complete the function that calculates the area of the red square, when the length of the circular arc A is given as the input.
+//      Return the result rounded to two decimals.
+//      Note: use the π value provided in your language (Math::PI, M_PI, math.pi, etc)
+
+public class AreaOfASquare {
+    public static double squareArea(double A) {
+        return 0;
+    }
+}
