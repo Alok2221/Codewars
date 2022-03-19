7e940ffc767c63080c7d403d3902d476d8fd1532
Aleks <aleksskela591@gmail.com>
2022-03-19 01:03:03 +0100

    battle of the characters easy

---
+++ src/main/java/com/codewars/sevenkyu/BattleOfTheCharactersEasy.java
@@ -0,0 +1,21 @@
+package com.codewars.sevenkyu;
+
+//      Description:
+//      Groups of characters decided to make a battle. Help them to figure out which group is more powerful.
+//      Create a function that will accept 2 strings and return the one who's stronger.
+//
+//      Rules:
+//      Each character have its own power: A = 1, B = 2, ... Y = 25, Z = 26
+//      Strings will consist of uppercase letters only
+//      Only two groups to a fight.
+//      Group whose total power (A + B + C + ...) is bigger wins.
+//      If the powers are equal, it's a tie.
+//      Examples:
+//      battle("ONE", "TWO"); // => "TWO"`
+//      battle("ONE", "NEO"); // => "Tie!"
+
+public class BattleOfTheCharactersEasy {
+    public static String battle(final String x, final String y) {
+        return ""; // Let the battle begin!
+    }
+}
