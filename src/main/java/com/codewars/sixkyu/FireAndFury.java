e45e96611957268f60c2481feb90b7bb47d57d80
Aleks <aleksskela591@gmail.com>
2022-03-19 02:05:22 +0100

    solution

--- src/main/java/com/codewars/sixkyu/FireAndFury.java
+++ src/main/java/com/codewars/sixkyu/FireAndFury.java
@@ -27,7 +27,15 @@ package com.codewars.sixkyu;
 //      ex3. FYRYFIRUFIRUFURE = "Fake tweet."
 
 public class FireAndFury {
-    public static String fireAndFury(final String tweet) {
-        return "Fake tweet.";
+    public static String fireAndFury(String tweet) {
+        if (tweet.matches(".*[^EFIRUY].*") || !tweet.matches(".*?(FURY|FIRE).*")) {
+            return "Fake tweet.";
+        }
+        tweet = tweet.replace("FIRE", "1").replace("FURY", "2").replaceAll("[A-Z]", "");
+        for (int i = tweet.length(); i > 0; i--) {
+            tweet = tweet.replaceAll("1".repeat(i), " You " + "and you ".repeat(i - 1) + "are fired!")
+                    .replaceAll("2".repeat(i), " I am " + "really ".repeat(i - 1) + "furious.");
+        }
+        return tweet.trim();
     }
 }
