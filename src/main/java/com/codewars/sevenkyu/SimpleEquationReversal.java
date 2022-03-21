c4fde3a6cefa515873459b53b80dba1eb9d8af66
Aleks <aleksskela591@gmail.com>
2022-03-21 10:26:13 +0100

    simple equation reversal

---
+++ src/main/java/com/codewars/sevenkyu/SimpleEquationReversal.java
@@ -0,0 +1,13 @@
+package com.codewars.sevenkyu;
+
+//      Given a mathematical equation that has *,+,-,/, reverse it as follows:
+//      solve("100*b/y") = "y/b*100"
+//      solve("a+b-c/d*30") = "30*d/c-b+a"
+//      More examples in test cases.
+//      Good luck!
+
+public class SimpleEquationReversal {
+    public static String solve(String eq) {
+        return "";
+    }
+}
