88c3ef5d9146b808f7c15d0d7dd1e15071afe7bf
Aleks <aleksskela591@gmail.com>
2022-03-24 21:39:56 +0100

    beginner lost without a map

---
+++ src/main/java/com/codewars/eightkyu/BeginnerLostWithoutAMap.java
@@ -0,0 +1,11 @@
+package com.codewars.eightkyu;
+
+//      Given an array of integers, return a new array with each value doubled.
+//      For example:
+//      [1, 2, 3] --> [2, 4, 6]
+
+public class BeginnerLostWithoutAMap {
+    public static int[] map(int[] arr) {
+        return new int[]{};
+    }
+}
