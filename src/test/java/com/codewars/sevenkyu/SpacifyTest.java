5f3f75596249f879999d742fdee973642a5ae3a1
Aleks <aleksskela591@gmail.com>
2022-03-24 22:13:39 +0100

    add test

---
+++ src/test/java/com/codewars/sevenkyu/SpacifyTest.java
@@ -0,0 +1,13 @@
+package com.codewars.sevenkyu;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class SpacifyTest {
+    @Test
+    public void basicTest() {
+        assertEquals("h e l l o   w o r l d", Spacify.spacify("hello world"));
+        assertEquals("1 2 3 4 5", Spacify.spacify("12345"));
+    }
+}
