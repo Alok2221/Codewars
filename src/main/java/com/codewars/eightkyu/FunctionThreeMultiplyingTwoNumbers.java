19227ad09a5c8db18dea8c112a353487e6cbdeb5
Aleks <aleksskela591@gmail.com>
2022-03-24 21:28:50 +0100

    function 3 - multiplying two numbers

---
+++ src/main/java/com/codewars/eightkyu/FunctionThreeMultiplyingTwoNumbers.java
@@ -0,0 +1,9 @@
+package com.codewars.eightkyu;
+
+//      Implement a function which multiplies two numbers.
+
+public class FunctionThreeMultiplyingTwoNumbers {
+    public static int multiply(int num1, int num2) {
+        return 0;
+    }
+}
