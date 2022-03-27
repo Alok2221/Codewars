01b4135a0f7c9634265b05360b6ef06905090646
Aleks <aleksskela591@gmail.com>
2022-03-27 21:46:02 +0200

    digits explosion

---
+++ src/main/java/com/codewars/sevenkyu/DigitsExplosion.java
@@ -0,0 +1,16 @@
+package com.codewars.sevenkyu;
+//      Given a string made of digits [0-9], return a string where each digit is repeated a number of times equals to its value.
+//      Examples
+//      explode("312")
+//      should return :
+//      "333122"
+//
+//      explode("102269")
+//      should return :
+//      "12222666666999999999"
+
+public class DigitsExplosion {
+    public static String explode(String digits) {
+        return digits;
+    }
+}
