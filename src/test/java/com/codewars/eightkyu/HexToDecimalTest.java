e806ddf6e061b636afb73f40197a34b6f6911cf8
Aleks <aleksskela591@gmail.com>
2022-03-30 16:18:20 +0200

    add test

---
+++ src/test/java/com/codewars/eightkyu/HexToDecimalTest.java
@@ -0,0 +1,17 @@
+package com.codewars.eightkyu;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class HexToDecimalTest {
+
+    @Test
+    public void examples() {
+        assertEquals(1, HexToDecimal.hexToDec("1"));
+        assertEquals(10, HexToDecimal.hexToDec("a"));
+        assertEquals(16, HexToDecimal.hexToDec("10"));
+        assertEquals(255, HexToDecimal.hexToDec("FF"));
+        assertEquals(-12, HexToDecimal.hexToDec("-C"));
+    }
+}
