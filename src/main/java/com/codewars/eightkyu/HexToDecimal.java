74bcae7c3a50656771892ae10cdd81583bd96c7e
Aleks <aleksskela591@gmail.com>
2022-03-30 16:18:07 +0200

    hex to decimal

---
+++ src/main/java/com/codewars/eightkyu/HexToDecimal.java
@@ -0,0 +1,9 @@
+package com.codewars.eightkyu;
+
+//      Complete the function which converts hex number (given as a string) to a decimal number.
+
+public class HexToDecimal {
+    public static int hexToDec(final String hexString) {
+        return 0;
+    }
+}
