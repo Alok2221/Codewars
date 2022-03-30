b1c98eed42a0f7796e8cf7da0317a98ad8dae4d1
Aleks <aleksskela591@gmail.com>
2022-03-30 16:14:24 +0200

    enumerable magic take the first N elements

---
+++ src/main/java/com/codewars/eightkyu/EnumerableMagicTakeTheFirstNElements.java
@@ -0,0 +1,13 @@
+package com.codewars.eightkyu;
+
+//      Create a function that accepts a list/array and a number n, and returns a list/array of the first n elements from the list/array.
+//      If you need help, here's a reference:
+//      https://docs.oracle.com/javase/6/docs/api/java/util/Arrays.html#copyOfRange(int[],%20int,%20int)
+
+import java.util.Arrays;
+
+public class EnumerableMagicTakeTheFirstNElements {
+    public static int[] take(int[] arr, int n) {
+        return arr;
+    }
+}
