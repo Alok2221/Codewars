5291cbd511a8d9916be26302468c02e7da09493d
Aleks <aleksskela591@gmail.com>
2022-04-04 11:55:55 +0200

    beginner series three sum of numbers

---
+++ src/main/java/com/codewars/sevenkyu/BeginnerSeriesThreeSumOfNumbers.java
@@ -0,0 +1,19 @@
+package com.codewars.sevenkyu;
+
+//      Given two integers a and b, which can be positive or negative, find the sum of all the integers between and including them and return it.
+//      If the two numbers are equal return a or b.
+//      Note: a and b are not ordered!
+//      Examples (a, b) --> output (explanation)
+//      (1, 0) --> 1 (1 + 0 = 1)
+//      (1, 2) --> 3 (1 + 2 = 3)
+//      (0, 1) --> 1 (0 + 1 = 1)
+//      (1, 1) --> 1 (1 since both are same)
+//      (-1, 0) --> -1 (-1 + 0 = -1)
+//      (-1, 2) --> 2 (-1 + 0 + 1 + 2 = 2)
+
+
+public class BeginnerSeriesThreeSumOfNumbers {
+    public int GetSum(int a, int b) {
+        return 0;
+    }
+}
