d7e23f8d0ba2ecca1cc64370a2163ca33da4177f
Aleks <aleksskela591@gmail.com>
2022-04-06 10:36:48 +0200

    insert dashes

---
+++ src/main/java/com/codewars/sevenkyu/InsertDashes.java
@@ -0,0 +1,12 @@
+package com.codewars.sevenkyu;
+
+//      Write a function insert_dash(num) / insertDash(num) / InsertDash(int num) that will insert dashes ('-') between each two odd digits in num.
+//      For example: if num is 454793 the output should be 4547-9-3.
+//      Don't count zero as an odd digit.
+//      Note that the number will always be non-negative (>= 0).
+
+public class InsertDashes {
+    public static String insertDash(int num) {
+        return "";
+    }
+}
