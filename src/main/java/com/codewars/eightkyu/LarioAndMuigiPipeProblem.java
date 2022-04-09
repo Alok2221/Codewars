9a691dc8ed7450c412832b6bb5b7bfb515de3802
Aleks <aleksskela591@gmail.com>
2022-04-09 17:35:45 +0200

    solution

--- src/main/java/com/codewars/eightkyu/LarioAndMuigiPipeProblem.java
+++ src/main/java/com/codewars/eightkyu/LarioAndMuigiPipeProblem.java
@@ -10,6 +10,6 @@ import java.util.stream.IntStream;
 
 public class LarioAndMuigiPipeProblem {
     public static int[] pipeFix(int[] numbers) {
-        return null;
+        return IntStream.rangeClosed(numbers[0], numbers[numbers.length - 1]).toArray();
     }
 }
