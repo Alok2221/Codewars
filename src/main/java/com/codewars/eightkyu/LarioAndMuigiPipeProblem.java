3f020658c39ce4e1f51327fcf4a1acab7c4016e0
Aleks <aleksskela591@gmail.com>
2022-04-09 17:34:06 +0200

    lario And muigi pipe problem

---
+++ src/main/java/com/codewars/eightkyu/LarioAndMuigiPipeProblem.java
@@ -0,0 +1,15 @@
+package com.codewars.eightkyu;
+
+//      Task
+//      Given the a list of numbers, return the list so that the values increment by 1 for each index up to the maximum value.
+//      Example
+//      Input: 1,3,5,6,7,8
+//      Output: 1,2,3,4,5,6,7,8
+
+import java.util.stream.IntStream;
+
+public class LarioAndMuigiPipeProblem {
+    public static int[] pipeFix(int[] numbers) {
+        return null;
+    }
+}
