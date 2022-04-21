b55bcb8d6383075988ed5b421e4370390067cbab
Aleks <aleksskela591@gmail.com>
2022-04-21 22:51:34 +0200

    wilson primes

---
+++ src/main/java/com/codewars/eightkyu/WilsonPrimes.java
@@ -0,0 +1,11 @@
+package com.codewars.eightkyu;
+
+//      Wilson primes satisfy the following condition. Let P represent a prime number.
+//      Then ((P-1)! + 1) / (P * P) should give a whole number.
+//      Your task is to create a function that returns true if the given number is a Wilson prime.
+
+public class WilsonPrimes {
+    public static boolean am_i_wilson(double n) {
+        return false;
+    }
+}
