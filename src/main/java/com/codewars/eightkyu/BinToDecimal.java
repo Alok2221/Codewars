9a4405f3e1a5b15487b04b53ddaf0b59cf1657df
Aleks <aleksskela591@gmail.com>
2022-04-23 12:53:35 +0200

    bin to decimal

---
+++ src/main/java/com/codewars/eightkyu/BinToDecimal.java
@@ -0,0 +1,9 @@
+package com.codewars.eightkyu;
+
+//      Complete the function which converts a binary number (given as a string) to a decimal number.
+
+public class BinToDecimal {
+    public static int binToDecimal(String inp) {
+        return 0;
+    }
+}
