cebeb246418086ef8cfaf0d94d115904029b274b
Aleks <aleksskela591@gmail.com>
2022-04-25 10:34:01 +0200

    binary score

---
+++ src/main/java/com/codewars/sevenkyu/BinaryScore.java
@@ -0,0 +1,19 @@
+package com.codewars.sevenkyu;
+
+//      Objective
+//      Given a number n we will define its scORe to be 0 | 1 | 2 | 3 | ... | n, where | is the bitwise OR operator.
+//      Write a function that takes n and finds it's scORe.
+//      n	scORe n
+//      0	0
+//      1	1
+//      49	63
+//      1000000	1048575
+//      Any feedback would be much appreciated
+
+import java.math.BigInteger;
+
+public class BinaryScore {
+    public static BigInteger score(BigInteger n) {
+        return new BigInteger("0");
+    }
+}
