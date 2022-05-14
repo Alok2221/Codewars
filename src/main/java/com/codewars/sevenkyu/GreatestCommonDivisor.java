78151d3964bcec226a655b11145e813d8fc1db23
Aleks <aleksskela591@gmail.com>
2022-05-14 16:09:50 +0200

    solution

--- src/main/java/com/codewars/sevenkyu/GreatestCommonDivisor.java
+++ src/main/java/com/codewars/sevenkyu/GreatestCommonDivisor.java
@@ -5,6 +5,12 @@ package com.codewars.sevenkyu;
 
 public class GreatestCommonDivisor {
     public static int compute(int x, int y) {
-        return 0;
+        int gcd = 1;
+        for (int i = 1; i <= x && i <= y; i++) {
+            if (x % i == 0 && y % i == 0) {
+                gcd = i;
+            }
+        }
+        return gcd;
     }
 }
