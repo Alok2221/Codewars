d103ad71d65dccc3bdc456ed28f4b57601fe2de3
Aleks <aleksskela591@gmail.com>
2022-05-23 23:22:41 +0200

    solution

--- src/main/java/com/codewars/sevenkyu/BingoOrNot.java
+++ src/main/java/com/codewars/sevenkyu/BingoOrNot.java
@@ -4,8 +4,18 @@ package com.codewars.sevenkyu;
 //      Each number corresponds to their alphabetical order letter (e.g. 1 = A. 2 = B, etc). Write a function where you will win the game if your numbers can spell "BINGO".
 //      They do not need to be in the right order in the input array. Otherwise you will lose. Your outputs should be "WIN" or "LOSE" respectively.
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class BingoOrNot {
-    public static String bingo(int[] numberArray){
-        return "";
+    public static String bingo(int[] numberArray) {
+        List<Integer> list = new ArrayList<>();
+        for (int j : numberArray) {
+            list.add(j);
+        }
+        if (list.contains(2) && list.contains(9) && list.contains(14) && list.contains(7) && list.contains(15)) {
+            return "WIN";
+        }
+        return "LOSE";
     }
 }
