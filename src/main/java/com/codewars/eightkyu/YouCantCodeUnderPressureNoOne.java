fb58d79d21fa31850ac09425d1c8d7cda8ccbbc6
Aleks <aleksskela591@gmail.com>
2022-05-23 22:52:41 +0200

    you can't code under pressure #1

---
+++ src/main/java/com/codewars/eightkyu/YouCantCodeUnderPressureNoOne.java
@@ -0,0 +1,9 @@
+package com.codewars.eightkyu;
+
+//      Code as fast as you can! You need to double the integer and return it.
+
+public class YouCantCodeUnderPressureNoOne {
+    public static int doubleInteger(int i) {
+        return i;
+    }
+}
