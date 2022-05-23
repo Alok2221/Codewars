c7f6963e3011d5a9541dee2a95ab357b78df6d39
Aleks <aleksskela591@gmail.com>
2022-05-23 22:52:57 +0200

    add test

---
+++ src/test/java/com/codewars/eightkyu/YouCantCodeUnderPressureNoOneTest.java
@@ -0,0 +1,11 @@
+package com.codewars.eightkyu;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class YouCantCodeUnderPressureNoOneTest {
+    @Test
+    public final void test_all() {
+        Assert.assertEquals("Nope!", 4, YouCantCodeUnderPressureNoOne.doubleInteger(2)); // Test cases here!
+    }
+}
