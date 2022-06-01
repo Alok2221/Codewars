e328b0ece68b6e0f52dab30f1a8316d0847ef910
Aleks <aleksskela591@gmail.com>
2022-06-01 22:08:04 +0200

    add test

---
+++ src/test/java/com/codewars/eightkyu/DalmatiansSquashTheBugsNotTheDogsTest.java
@@ -0,0 +1,18 @@
+package com.codewars.eightkyu;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class DalmatiansSquashTheBugsNotTheDogsTest {
+
+    @Test
+    public void basicTests() {
+        assertEquals("More than a handful!", DalmatiansSquashTheBugsNotTheDogs.howManyDalmatians(26));
+        assertEquals("Hardly any", DalmatiansSquashTheBugsNotTheDogs.howManyDalmatians(8));
+        assertEquals("More than a handful!", DalmatiansSquashTheBugsNotTheDogs.howManyDalmatians(14));
+        assertEquals("Woah that's a lot of dogs!", DalmatiansSquashTheBugsNotTheDogs.howManyDalmatians(80));
+        assertEquals("Woah that's a lot of dogs!", DalmatiansSquashTheBugsNotTheDogs.howManyDalmatians(100));
+        assertEquals("101 DALMATIANS!!!", DalmatiansSquashTheBugsNotTheDogs.howManyDalmatians(101));
+    }
+}
