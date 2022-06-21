ed77ecb4f43d3378ebe818d4597f307ade4e962f
Aleks <aleksskela591@gmail.com>
2022-06-21 23:41:06 +0200

    solution

--- src/main/java/com/codewars/eightkyu/DalmatiansSquashTheBugsNotTheDogs.java
+++ src/main/java/com/codewars/eightkyu/DalmatiansSquashTheBugsNotTheDogs.java
@@ -6,6 +6,7 @@ package com.codewars.eightkyu;
 
 public class DalmatiansSquashTheBugsNotTheDogs {
     public static String howManyDalmatians(int number) {
-        return null;
+        String[] dogs = new String[]{"Hardly any", "More than a handful!", "Woah that's a lot of dogs!", "101 DALMATIANS!!!"};
+        return number <= 10 ? dogs[0] : number <= 50 ? dogs[1] : number == 101 ? dogs[3] : dogs[2];
     }
 }
