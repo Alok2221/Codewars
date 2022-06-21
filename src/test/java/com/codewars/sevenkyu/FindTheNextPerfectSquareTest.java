1c4c5767659c80025dfbd7f7f28941e6b29b13a0
Aleks <aleksskela591@gmail.com>
2022-06-21 23:46:21 +0200

    add test

---
+++ src/test/java/com/codewars/sevenkyu/FindTheNextPerfectSquareTest.java
@@ -0,0 +1,23 @@
+package com.codewars.sevenkyu;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+
+public class FindTheNextPerfectSquareTest {
+    @Test
+    public void test1() {
+        assertEquals(144, FindTheNextPerfectSquare.findNextSquare(121));
+    }
+
+    @Test
+    public void test2() {
+        assertEquals(676, FindTheNextPerfectSquare.findNextSquare(625));
+    }
+
+    @Test
+    public void test3() {
+        assertEquals(-1, FindTheNextPerfectSquare.findNextSquare(114));
+    }
+}
