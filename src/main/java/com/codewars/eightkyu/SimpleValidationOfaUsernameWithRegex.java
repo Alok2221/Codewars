6543b2568fd486a3909772ac0aed0d431ee29cc2
Aleks <aleksskela591@gmail.com>
2022-06-22 22:59:43 +0200

    simple validation of a username with regex

---
+++ src/main/java/com/codewars/eightkyu/SimpleValidationOfaUsernameWithRegex.java
@@ -0,0 +1,13 @@
+package com.codewars.eightkyu;
+
+//      Write a simple regex to validate a username. Allowed characters are:
+//      lowercase letters,
+//      numbers,
+//      underscore
+//      Length should be between 4 and 16 characters (both included).
+
+public class SimpleValidationOfaUsernameWithRegex {
+    public static boolean validateUsr(String s) {
+        return false;
+    }
+}
