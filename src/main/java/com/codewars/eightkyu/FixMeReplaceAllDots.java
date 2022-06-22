83c767ded6056b65b39832a3fc33ec2b6ad27a2d
Aleks <aleksskela591@gmail.com>
2022-06-22 22:53:39 +0200

    FIXME: replace all dots

---
+++ src/main/java/com/codewars/eightkyu/FixMeReplaceAllDots.java
@@ -0,0 +1,14 @@
+package com.codewars.eightkyu;
+
+//      The code provided is supposed replace all the dots . in the specified String str with dashes -
+//      But it's not working properly.
+//      Task
+//      Fix the bug so we can all go home early.
+//      Notes
+//      String str will never be null.
+
+public class FixMeReplaceAllDots {
+    public static String replaceDots(final String str) {
+        return str.replaceAll(".", "-");
+    }
+}
