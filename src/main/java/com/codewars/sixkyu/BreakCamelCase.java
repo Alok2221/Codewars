9c5bf4eb9aee91e01545239c7db5fcbde38aeeca
Aleks <aleksskela591@gmail.com>
2022-07-19 23:59:43 +0200

    break camelCase

---
+++ src/main/java/com/codewars/sixkyu/BreakCamelCase.java
@@ -0,0 +1,14 @@
+package com.codewars.sixkyu;
+
+//      Complete the solution so that the function will break up camel casing, using a space between words.
+//      Example
+//      "camelCasing"  =>  "camel Casing"
+//      "identifier"   =>  "identifier"
+//      ""             =>  ""
+
+
+public class BreakCamelCase {
+    public static String camelCase(String input) {
+        return null;
+    }
+}
