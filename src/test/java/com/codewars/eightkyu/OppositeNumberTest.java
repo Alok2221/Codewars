2a70e94f1b920d30fbd166726b5afc6136df4026
Aleks <aleksskela591@gmail.com>
2022-07-20 00:22:54 +0200

    add test

---
+++ src/test/java/com/codewars/eightkyu/OppositeNumberTest.java
@@ -0,0 +1,12 @@
+package com.codewars.eightkyu;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class OppositeNumberTest {
+    @Test
+    public void tests() {
+        assertEquals(-1, OppositeNumber.opposite(1));
+    }
+}
