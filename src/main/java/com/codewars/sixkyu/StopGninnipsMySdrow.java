8c9b20d44cac55df39867d5bbcaa399499d14379
Aleks <aleksskela591@gmail.com>
2022-07-27 00:51:11 +0200

    solution

--- src/main/java/com/codewars/sixkyu/StopGninnipsMySdrow.java
+++ src/main/java/com/codewars/sixkyu/StopGninnipsMySdrow.java
@@ -10,6 +10,12 @@ package com.codewars.sixkyu;
 
 public class StopGninnipsMySdrow {
     public String spinWords(String sentence) {
-        return null;
+        String[] words = sentence.split(" ");
+        for (int i = 0; i < words.length; i++) {
+            if (words[i].length() >= 5) {
+                words[i] = new StringBuilder(words[i]).reverse().toString();
+            }
+        }
+        return String.join(" ", words);
     }
 }
