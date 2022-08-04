39089c5051ddb11c72d02025c552589234bdfa83
Aleks <aleksskela591@gmail.com>
2022-08-04 22:27:54 +0200

    switcheroo

---
+++ src/test/java/com/codewars/sevenkyu/SwitcherooTest.java
@@ -0,0 +1,14 @@
+package com.codewars.sevenkyu;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class SwitcherooTest {
+    @Test
+    public void testSomething() {
+        assertEquals("abc", Switcheroo.switcheroo("bac"));
+        assertEquals("ccc", Switcheroo.switcheroo("ccc"));
+        assertEquals("aaabcccbaaa", Switcheroo.switcheroo("bbbacccabbb"));
+    }
+}
