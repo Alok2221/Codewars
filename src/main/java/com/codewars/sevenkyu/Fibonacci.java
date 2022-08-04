62c2366dc005e8f7a61931fb92b90767d260e535
Aleks <aleksskela591@gmail.com>
2022-08-04 23:12:56 +0200

    fibonacci

---
+++ src/main/java/com/codewars/sevenkyu/Fibonacci.java
@@ -0,0 +1,9 @@
+package com.codewars.sevenkyu;
+
+//      Create function fib that returns n'th element of Fibonacci sequence (classic programming task).
+
+public class Fibonacci {
+    public static long fib(int n) {
+        return 0;
+    }
+}
