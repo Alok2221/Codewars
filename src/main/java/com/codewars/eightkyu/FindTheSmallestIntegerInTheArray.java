9af9ff60d0d4edf11e9624766badcc8a0f46a281
Aleks <aleksskela591@gmail.com>
2022-08-04 22:25:25 +0200

    solution

--- src/main/java/com/codewars/eightkyu/FindTheSmallestIntegerInTheArray.java
+++ src/main/java/com/codewars/eightkyu/FindTheSmallestIntegerInTheArray.java
@@ -6,8 +6,13 @@ package com.codewars.eightkyu;
 //      Given [34, -345, -1, 100] your solution will return -345
 //      You can assume, for the purpose of this kata, that the supplied array will not be empty.
 
+import java.util.stream.IntStream;
+
 public class FindTheSmallestIntegerInTheArray {
     public static int findSmallestInt(int[] args) {
+        if (IntStream.of(args).min().isPresent()) {
+            return IntStream.of(args).min().getAsInt();
+        }
         return 0;
     }
 }
