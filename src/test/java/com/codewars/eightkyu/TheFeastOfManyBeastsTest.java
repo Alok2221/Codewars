6f6ec42b3e4d5c5ee6fe68a85232a208d2da2517
Aleks <aleksskela591@gmail.com>
2022-09-23 00:14:46 +0200

    the feast of many beasts

---
+++ src/test/java/com/codewars/eightkyu/TheFeastOfManyBeastsTest.java
@@ -0,0 +1,15 @@
+package com.codewars.eightkyu;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class TheFeastOfManyBeastsTest {
+    @Test
+    public void fixedTest() {
+        assertTrue(TheFeastOfManyBeasts.feast("great blue heron", "garlic nann"));
+        assertTrue(TheFeastOfManyBeasts.feast("chickadee", "chocolate cake"));
+        assertFalse(TheFeastOfManyBeasts.feast("brown bear", "bear claw"));
+    }
+}
