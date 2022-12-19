ebf7bca7d062597385de7ef713fc9ffeb7b871ed
Aleks <aleksskela591@gmail.com>
2022-12-19 22:21:31 +0100

    solution

--- src/main/java/com/codewars/eightkyu/LocalizeTheBarycenterOfATriangle.java
+++ src/main/java/com/codewars/eightkyu/LocalizeTheBarycenterOfATriangle.java
@@ -15,7 +15,10 @@ package com.codewars.eightkyu;
 //      Enjoy it and happy coding!!
 
 public class LocalizeTheBarycenterOfATriangle {
-    public static double[] barTriang(double[] x, double[] y, double[] z){
-        return null;
+    public static double[] barTriang(double[] x, double[] y, double[] z) {
+        double[] res = new double[2];
+        res[0] = (double) ((int) Math.round((x[0] + y[0] + z[0]) / 3 * 10000)) / 10000;
+        res[1] = (double) ((int) Math.round((x[1] + y[1] + z[1]) / 3 * 10000)) / 10000;
+        return res;
     }
 }
