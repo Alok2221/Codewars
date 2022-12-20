61012d054c5f6d9a7cb530eb981b4f1a360afff1
Aleks <aleksskela591@gmail.com>
2022-12-20 17:12:12 +0100

    solution

--- src/main/java/com/codewars/sevenkyu/SurviveTheAttack.java
+++ src/main/java/com/codewars/sevenkyu/SurviveTheAttack.java
@@ -23,9 +23,14 @@ package com.codewars.sevenkyu;
 //      1 survivors                3 survivors
 //      return true
 
+import static java.util.stream.IntStream.of;
 
 public class SurviveTheAttack {
-    public static boolean block(int[] attackers, int[] defenders){
-        return false;
+    public static boolean block(int[] attackers, int[] defenders) {
+        int survived = defenders.length - attackers.length;
+        for (int i = 0; i < Math.min(defenders.length, attackers.length); i++) {
+            survived += Math.signum(defenders[i] - attackers[i]);
+        }
+        return survived > 0 || survived == 0 && of(defenders).sum() >= of(attackers).sum();
     }
 }
