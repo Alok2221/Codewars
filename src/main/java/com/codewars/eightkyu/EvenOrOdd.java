8b9087c92effb05a7b5ce481c84f89c93c209660
Aleks <aleksskela591@gmail.com>
2023-02-09 18:18:36 +0100

    even or odd

---
+++ src/main/java/com/codewars/eightkyu/EvenOrOdd.java
@@ -0,0 +1,10 @@
+package com.codewars.eightkyu;
+
+//      Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+
+public class EvenOrOdd {
+    public static String even_or_odd(int number) {
+        return null;
+    }
+
+}
