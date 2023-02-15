65152ab84d77e0fa0ef861329805de0d5a289b99
Aleks <aleksskela591@gmail.com>
2023-02-15 23:43:45 +0100

    decompose a number

---
+++ src/test/java/com/codewars/sixkyu/DecomposeANumberTest.java
@@ -0,0 +1,36 @@
+package com.codewars.sixkyu;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+
+import static org.junit.Assert.assertEquals;
+
+
+public class DecomposeANumberTest {
+
+    private static void testing(String actual, String expected) {
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void test() {
+        System.out.println("Fixed Tests decompose");
+        long[][] ans = DecomposeANumber.decompose(0);
+        String ansStr = Arrays.toString(ans[0]) + Arrays.toString(ans[1]);
+        testing(ansStr, "[][0]");
+        ans = DecomposeANumber.decompose(4);
+        ansStr = Arrays.toString(ans[0]) + Arrays.toString(ans[1]);
+        testing(ansStr, "[2][0]");
+        ans = DecomposeANumber.decompose(9);
+        ansStr = Arrays.toString(ans[0]) + Arrays.toString(ans[1]);
+        testing(ansStr, "[3][1]");
+        ans = DecomposeANumber.decompose(25);
+        ansStr = Arrays.toString(ans[0]) + Arrays.toString(ans[1]);
+        testing(ansStr, "[4, 2][0]");
+        ans = DecomposeANumber.decompose(45);
+        ansStr = Arrays.toString(ans[0]) + Arrays.toString(ans[1]);
+        testing(ansStr, "[5, 2][4]");
+    }
+
+}
