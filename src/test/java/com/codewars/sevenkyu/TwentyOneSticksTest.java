f3173406a05f97707f7fa86f4fe3789dc01b055b
aleks <aleksskela591@gmail.com>
2023-02-26 23:35:39 +0100

    21 Sticks

---
+++ src/test/java/com/codewars/sevenkyu/TwentyOneSticksTest.java
@@ -0,0 +1,13 @@
+package com.codewars.sevenkyu;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class TwentyOneSticksTest {
+    @Test
+    public void sampleTest() {
+        assertEquals(3, TwentyOneSticks.makeMove(3));
+    }
+
+}
