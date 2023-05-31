46286b59c3eb9a0cc324073f02e7fa0d71f295df
aleks <aleksskela591@gmail.com>
2023-05-31 23:50:09 +0200

    write number in expanded form

--- src/main/java/com/codewars/sixkyu/WriteNumberInExpandedForm.java
+++ src/main/java/com/codewars/sixkyu/WriteNumberInExpandedForm.java
@@ -8,9 +8,23 @@ package com.codewars.sixkyu;
         Kata.expandedForm(70304); # Should return "70000 + 300 + 4"
         NOTE: All numbers will be whole numbers greater than 0.*/
 
-public class WriteNumberInExpandedForm {
-    public static String expandedForm(int num){
-        return null;
-    }
+import java.util.Arrays;
 
+public class WriteNumberInExpandedForm {
+    public static String expandedForm(int num) {
+        String[] str = Integer.toString(num).split("");
+        for (int i = 0; i < str.length - 1; i++) {
+            if (Integer.parseInt(str[i]) > 0) {
+                for (int j = i; j < str.length - 1; j++) {
+                    str[i] += '0';
+                }
+            }
+        }
+        String result = Arrays.toString(str);
+        result = result
+                .substring(1, result.length() - 1)
+                .replace(", 0", "")
+                .replace(",", " +");
+        return result;
+    }
 }
