303298eff3776f03feeed50cb7af6c2081ebf971
aleks <aleksskela591@gmail.com>
2025-04-25 23:29:54 +0200

    Adding Big Numbers without using Math library

--- src/main/java/com/codewars/fourkyu/AddingBigNumbers.java
+++ src/main/java/com/codewars/fourkyu/AddingBigNumbers.java
@@ -11,10 +11,23 @@ package com.codewars.fourkyu;
 //  The input is a string of only digits
 //  The numbers are positives
 
-import java.math.BigInteger;
-
 public class AddingBigNumbers {
     public static String add(String a, String b) {
-        return new BigInteger(a).add(new BigInteger(b)).toString();
+        StringBuilder result = new StringBuilder();
+
+        int i = a.length() - 1;
+        int j = b.length() - 1;
+        int carry = 0;
+
+        while (i >= 0 || j >= 0 || carry > 0) {
+            int digitA = i >= 0 ? a.charAt(i--) - '0' : 0;
+            int digitB = j >= 0 ? b.charAt(j--) - '0' : 0;
+
+            int sum = digitA + digitB + carry;
+            result.append(sum % 10);
+            carry = sum / 10;
+        }
+
+        return result.reverse().toString();
     }
 }
